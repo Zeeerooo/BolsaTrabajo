dcf97145fe004a5252251d2ad71c0c9fe75c6d13
Boris <boris.rq@gmail.com>
2014-04-21 16:33:33 -0300

    Agregado views, corriendo template en /

--- public/views.py
+++ public/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.views.decorators.http import require_GET
+from django.template import RequestContext
 
-# Create your views here.
+@require_GET
+def home(request):
+    '''
+        index invoca a la pagina index.html, la pagina de inicio
+    '''
+    return render_to_response('index.html', context_instance = RequestContext(request))
