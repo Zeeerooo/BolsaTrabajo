f0f5095ec316742d32a34283bdd10cbf87649c8d
ommirandap <ommirandap@gmail.com>
2014-05-13 01:48:45 -0400

    Commit test on dev branch

--- public/urls.py
+++ public/urls.py
@@ -5,6 +5,8 @@ import views
 METHOD_DISPATCHER = 'public.views_utils.method_dispatcher'
 
 urlpatterns = patterns('',
-    url(r'^login/', METHOD_DISPATCHER, {'GET': views.loginView, 'POST': views.loginView_post}, name='login'),#redirecciona a la vista del login
-    url(r'','public.views.index', name='index'),#redirecciona a la vista de la pagina contacto ubicada en la carpeta templates, llamando a la funcion adkintun en el archivo view.py
+    url(r'^login/', METHOD_DISPATCHER, {'GET': views.loginView, 'POST': views.loginView_post}, name='login'),
+# Redirecciona a la vista del login
+    url(r'','public.views.index', name='index'),
+# Redirecciona a la vista de la pagina contacto ubicada en la carpeta templates, llamando a la funcion adkintun (Ah, si si) en el archivo view.py
 )
