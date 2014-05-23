1a55446cf42cfbf80c303b98f0136a0524ed693a
Sebastian <sebablasko@gmail.com>
2014-05-23 19:50:24 -0400

    Avance en implementacion del template de catalogo

--- normal_user/urls.py
+++ normal_user/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url, include
 import views
 
 urlpatterns = patterns('',
+		url(r'logout/','normal_user.views.logout', name='logout'),
+		url(r'datos_usuario/','normal_user.views.user_preferences', name='user_preferences'),
     url(r'','normal_user.views.catalogo', name='catalogo'),
 )
